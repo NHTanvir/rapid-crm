c71b8f46959bf8b3535fe77cf82e2c3d722fafff
NHTanvir <naymulhasantanvir10@gmail.com>
2025-05-23 19:21:44 +0600

    init

---
+++ rapid-crm.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Rapid CRM
+ * Description: Rapid CRM 
+ * Plugin URI: https://naymul.com
+ * Author: NH Tanvir
+ * Author URI: https://naymul.com
+ * Version: 0.1
+ * Text Domain: rapid-crm
+ */
