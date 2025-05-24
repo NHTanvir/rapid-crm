26950a94e0030edc52025ecc2370d8620c21ea18
NHTanvir <naymulhasantanvir10@gmail.com>
2025-05-24 21:52:58 +0600

    add constant

--- rapid-crm.php
+++ rapid-crm.php
@@ -8,3 +8,12 @@
  * Version: 0.1
  * Text Domain: rapid-crm
  */
+
+defined( 'ABSPATH' ) || exit;
+
+define( 'RAPIDCRM_FILE', __FILE__ );
+define( 'RAPIDCRM_VERSION', '0.1' );
+define( 'RAPIDCRM_PLUGIN_DIR', plugin_dir_path( RAPIDCRM_FILE ) );
+define( 'RAPIDCRM_PLUGIN_URL', plugin_dir_url( RAPIDCRM_FILE ) );
+define( 'RAPIDCRM_ASSETS_URL', RAPIDCRM_PLUGIN_URL . 'assets/' );
+define( 'RAPIDCRM_SPA_URL', RAPIDCRM_PLUGIN_URL . 'spa/' );
