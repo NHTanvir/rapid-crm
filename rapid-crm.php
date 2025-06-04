95c3c4f97a512d98e3e4fc7781eaea3b88f9cdc8
NHTanvir <naymulhasantanvir10@gmail.com>
2025-06-04 23:31:16 +0600

    add composer file and vendor autoload

--- rapid-crm.php
+++ rapid-crm.php
@@ -9,6 +9,8 @@
  * Text Domain: rapid-crm
  */
 
+namespace RapidCRM;
+
 defined( 'ABSPATH' ) || exit;
 
 define( 'RAPIDCRM_FILE', __FILE__ );
@@ -17,3 +19,5 @@ define( 'RAPIDCRM_PLUGIN_DIR', plugin_dir_path( RAPIDCRM_FILE ) );
 define( 'RAPIDCRM_PLUGIN_URL', plugin_dir_url( RAPIDCRM_FILE ) );
 define( 'RAPIDCRM_ASSETS_URL', RAPIDCRM_PLUGIN_URL . 'assets/' );
 define( 'RAPIDCRM_SPA_URL', RAPIDCRM_PLUGIN_URL . 'spa/' );
+
+require_once 'vendor/autoload.php';
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'rapidcrm/rapid-crm',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '26950a94e0030edc52025ecc2370d8620c21ea18',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'rapidcrm/rapid-crm' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '26950a94e0030edc52025ecc2370d8620c21ea18',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
